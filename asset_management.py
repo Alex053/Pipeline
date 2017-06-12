c78f72d3b2d1d57af43625b1e1b3d7aa1c3a489f
Ramon Partida <jrpartida04@hotmail.com>
2017-06-11 22:09:07 -0500

    Segunda parte de la validacion

--- asset_management.py
+++ asset_management.py
@@ -11,16 +11,45 @@ print sg.base_url
 
 #Funciones de validacion
 def validateOption(userInput):
-	if(userInput == "preview"):
-		return "Preview"
-	elif(userInput == "shot"):
-		return "Shot"
-	else:
-		recurisveInput = raw_input("ERROR. El tipo de archivo no existe, vuelve a intentar\n Que quieres subir?\n>Shot\n>Preview\n").lower()
-		validateOption(recurisveInput)
+	while True:
+		if(userInput == "preview"):
+			return "Preview"
+		elif(userInput == "asset"):
+			return "Asset"
+		else:
+			userInput = raw_input("ERROR. El tipo de archivo no existe, vuelve a intentar\n Que quieres subir?\n>Asset\n>Preview\n").lower()
+			#validateOption(recurisveInput)
+
+def validateID(userInput):
+	while True:
+		try:
+			userInput = int(userInput)
+			return int(userInput)
+		except:
+			userInput = raw_input ("ERROR. El ID debe ser un numero entero\n Intentar otro valor\n")
+			#validateID(recurisveInput)
+
+def validateShotgunID(userInput):
+	while True:
+		result = sg.find_one(file_type, [["id", "is", userInput]], ["id", "code", "sg_status_list"])
+		if(result == None):
+			newInput = raw_input("El %s no tiene el ID %03d, intenta con otro numero\n" %(file_type, userInput))
+			try:
+				userInput = validateID(newInput)
+			except exception as e:
+				print e
+		else:
+			print ("El %s tiene el codigo: %s" %(file_type,result['code']))
+			return	result
 
 
+#Entrada inicial del usuario
 userOption = raw_input("Que tipo de archivo quieres trabajar?\n>Asset\n>Preview\n").lower()
 file_type = validateOption(userOption)
+#Usuario define el ID del elemento
+assetID = raw_input("Cual es el ID de tu archivo?\n")
+file_ID = validateID(assetID)
+#Revisamos que el elemento se encuentre dentro de shotgun
+shotgun_Info = validateShotgunID(file_ID)
 
 time.sleep(5)
